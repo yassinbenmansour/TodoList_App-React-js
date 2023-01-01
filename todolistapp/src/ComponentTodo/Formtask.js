1fde13419f82e1d602057d100625da2140674d53
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2023-01-01 04:19:29 +0100

    structure

--- todolistapp/src/ComponentTodo/Formtask.js
+++ todolistapp/src/ComponentTodo/Formtask.js
@@ -1,27 +1,47 @@
 import React, { useState } from 'react'
-import Task from './Task'
+import Task from './Task';
+import {v4 as uuidv4} from 'uuid';
 
 
 
 export default function Formtask() {
 
-    const [tache,SetTache] = useState([
-        {nom:"sport"},
-        {nom:"football"},
-        {nom:"coding session"}
+    const [tache,setTache] = useState([
+        {id:uuidv4(),nom:"sport"},
+        {id:uuidv4(),nom:"football"},
+        {id:uuidv4(),nom:"coding session"}
 
     ])
+
+    const [ztache , setZtache]= useState('');
+
+    const zidtache = () => {
+        let ntaches = [...tache];
+        let ntache = {} ;
+
+        ntache.id = uuidv4();
+        ntache.nom = ztache ;
+        ntaches.push(ntache);
+
+        setTache(ntaches);
+        setZtache("");
+    }
+
+
+
+
   return (
     <div>
 
         <div className="container mt-5 ">
 
         <h1>Todo List App</h1>
+        <form>
         Entre une tache :
-        <input type="text" className='form-control ' />
+        <input type="text" className='form-control '  onChange={(e)=>{setZtache(e.target.value)}} />
         <br />
-        <input type="button" className='btn btn-primary ' value="Ajouter " />
-        
+        <input type="button" onClick={zidtache} className='btn btn-primary ' value="Ajouter " />
+        </form>
         <br />
         <br />
         <h1>Todo List : Task</h1>
@@ -29,7 +49,7 @@ export default function Formtask() {
         <ul>
             {
                 tache.map((t) => {
-                    return <li> <Task txt={t.nom}/></li>
+                    return <li key={t.id}> <Task  txt={t.nom}/></li>
                 }) 
             }
         </ul>
--- todolistapp/src/ComponentTodo/Task.js
+++ todolistapp/src/ComponentTodo/Task.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+
+
+
 export default function Task({txt}) {
   return (
     <div >
         <div>
 
-              <input type="text" value={txt} readOnlyC/> <input type="button" className='btn btn-danger' value="Supprimer" />
+              <input type="text" value={txt} readOnly/> <input type="button" className='btn btn-danger' value="Supprimer" />
               
         </div>
     </div>
